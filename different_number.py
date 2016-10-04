22d540dffbfee56b76d71718d8de3a09b4585d6f
Martin Breining <martin.breining@gmail.com>
2016-10-03 19:39:05 -0700

    Add vim modeline

--- different_number.py
+++ different_number.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python
+# vim: foldlevel=0
 
 """
-https://www.pramp.com/question/aK6V5GVZ9MSPqvG1vwQp
-
 Given an array arr of n unique non-negative integers, how can you most efficiently find
 a non-negative integer that is not in the array?
 
 Your solution should return such an integer or null if arr contains all possible
 integers. Analyze the runtime and space complexity of your solution.
+
+https://www.pramp.com/question/aK6V5GVZ9MSPqvG1vwQp
 """
 from collections import defaultdict
 import sys
