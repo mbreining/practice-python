c21fd3d93df80076d26374a3447170ec50de0f75
Martin Breining <martin.breining@gmail.com>
2016-10-09 19:32:07 -0700

    Add solution for directed graph

--- detect_graph_cycles.py
+++ detect_graph_cycles.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+# vim: foldlevel=0
 
 """
 Given n nodes labeled from 0 to n - 1 and a list of undirected edges (each edge is a
@@ -10,6 +11,8 @@ undirected, [0, 1] is the same as [1, 0] and thus will not appear together in ed
 For example:
 Given n = 5 and edges = [[0, 1], [0, 2], [0, 3], [1, 4]], return true.
 Given n = 5 and edges = [[0, 1], [1, 2], [2, 3], [1, 3], [1, 4]], return false.
+
+Follow-up: Same but with a directed graph.
 
 http://nb4799.neu.edu/wordpress/?p=1143
 http://www.geeksforgeeks.org/detect-cycle-undirected-graph/
@@ -26,20 +29,19 @@ def find(parents, v):
     return v
 
 
-def solution1(n, edges):
+def undirected1(n, edges):
     """
-    >>> solution1(5, [[0, 1], [0, 2], [0, 3], [1, 4]])
+    Time complexity: O(Elog(V))
+    >>> undirected1(5, [[0, 1], [0, 2], [0, 3], [1, 4]])
     True
-    >>> solution1(5, [[0, 1], [1, 2], [2, 3], [1, 3], [1, 4]])
+    >>> undirected1(5, [[0, 1], [1, 2], [2, 3], [1, 3], [1, 4]])
     False
     """
     if len(edges) != n-1:
         return False
 
     # Initialize union find array
-    parents = []
-    for i in range(5):
-        parents[i] = -1
+    parents = [-1] * n
 
     # Check for cycles
     for v1, v2 in edges:
@@ -51,43 +53,97 @@ def solution1(n, edges):
     return True
 
 
-def dfs(vertex, vertices, recstack, visited):
-    recstack.append(vertex)
-    visited[vertex] = True
-    for v in vertices[vertex]:
-        if v in recstack:
-            return True
-        if not visited[v]:
-            dfs(v, vertices, recstack, visited)
-    recstack.pop()
+class Vertex(object):
+    def __init__(self, key):
+        self.key = key
+        self.adjlist = []
+        self.color = 0  # 0 for white, 1 for grey and 2 for black
+        self.parent = None
+
+
+def has_cycle(u, vertices):
+    u.color = 1  # visited
+    for v in u.adjlist:
+        if v.color == 0:  # not visited
+            v.parent = u
+            if has_cycle(v, vertices):
+                return True
+        else:  # vertex has already been visited
+            if v != u.parent:
+                return True  # we found a cycle
     return False
 
 
-def solution2(n, edges):
+def undirected2(n, edges):
     """
-    >>> solution2(5, [[0, 1], [0, 2], [0, 3], [1, 4]])
+    Time complexity: O(E+V)
+    >>> undirected2(5, [[0, 1], [0, 2], [0, 3], [1, 4]])
     True
-    >>> solution2(5, [[0, 1], [1, 2], [2, 3], [1, 3], [1, 4]])
+    >>> undirected2(5, [[0, 1], [1, 2], [2, 3], [1, 3], [1, 4]])
     False
     """
-    from collections import defaultdict
+    vertices = {}  # the graph
+    # Build graph
+    for i, j in edges:
+        if not vertices.get(i):
+            vertices[i] = Vertex(i)
+        u = vertices[i]
+        if not vertices.get(j):
+            vertices[j] = Vertex(j)
+        v = vertices[j]
+        u.adjlist.append(v)
+        v.adjlist.append(u)
 
-    # Build adjacency list
-    vertices = defaultdict(list)
-    for v1, v2 in edges:
-        vertices[v1].append(v2)
-        vertices[v2].append(v1)
-
-    # Check for cycles w/ DSF
-    recstack = []  # recursion stack
-    visited = []
-    for v in vertices:
-        visited[v] = False
-
-    for v in vertices:
-        if not visited[v]:
-            has_cycle = dfs(v, vertices, recstack, visited)
-            if has_cycle:
+    # Run DFS on graph
+    for _, vertex in vertices.iteritems():
+        if vertex.color == 0:  # not visited
+            if has_cycle(vertex, vertices):
                 return False
 
     return True
+
+
+def has_cycle2(u, vertices):
+    u.color = 1  # visited
+    for v in u.adjlist:
+        if v.color == 0:  # not visited
+            if has_cycle2(v, vertices):
+                return True
+        elif v.color == 1:  # visited but not processed yet
+            return True  # we found a cycle
+    u.color = 2  # processed
+    return False
+
+
+def directed(n, edges):
+    """
+    Time complexity: O(E+V)
+    >>> directed(5, [[0, 1], [0, 2], [0, 3], [1, 4]])
+    True
+    >>> directed(5, [[0, 1], [1, 2], [2, 3], [1, 3], [1, 4]])
+    True
+    >>> directed(5, [[0, 1], [1, 2], [2, 3], [1, 3], [1, 4], [3, 4], [4,0]])
+    False
+    """
+    vertices = {}  # the graph
+    # Build graph
+    for i, j in edges:
+        if not vertices.get(i):
+            vertices[i] = Vertex(i)
+        u = vertices[i]
+        if not vertices.get(j):
+            vertices[j] = Vertex(j)
+        v = vertices[j]
+        u.adjlist.append(v)  # directed
+
+    # Run DFS on graph
+    for _, vertex in vertices.iteritems():
+        if vertex.color == 0:  # not visited
+            if has_cycle2(vertex, vertices):
+                return False
+
+    return True
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
