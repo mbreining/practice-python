3b2bc12864d3f4097f015f529ff97d28e70f03dc
Martin Breining <martin.breining@gmail.com>
2016-10-17 12:38:34 -0700

    Refactor code

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,26 +1,24 @@
 #!/usr/bin/python
+# vim: foldlevel=0
 
 """
 Insertion sort
 """
-
 from random_array import randlist
 
 arr = randlist(50, 100)
 print "The input array: {0}".format(arr)
 
 
-def swap(arr, i, j):
-    arr[i], arr[j] = arr[j], arr[i]
+def sort(arr):
+    for i in range(1, len(arr)):
+        cur = arr[i]
+        j = i-1
+        while j >=0 and arr[j] > cur:
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = cur
 
 
-def insertion_sort(arr):
-    cur = 1
-    while cur < len(arr):
-        for idx, value in enumerate(reversed(arr[:cur])):
-            if arr[cur-idx] < value:
-                swap(arr, cur-idx-1, cur-idx)
-        cur += 1
-
-insertion_sort(arr)
+sort(arr)
 print "The sorted array: {0}".format(arr)
